03955c7042d4a5fd7df4ffffdc7a0459ece9a33a
Your Name <you@example.com>
2024-05-22 04:38:44 +0300

    Updates for every os

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os, shutil
 
+ 
+# Define folder names on where we will move files
+folder_names = ['Pdf files', 'Image files', 'Text files', 'Tuto files', 'APK files']
+
+# Define path to 'Downloads' directory
 path = r'C:/Users/scale/Downloads/'
-file_name = os.listdir(path)
 
+# Create folders if they don't exist
+for folder_name in folder_names:
+    folder_path = os.path.join(path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
+# Get list of file names in 'Downloads' directory
+file_names = os.listdir(path)
 
-
-folder_names = ['csv files', 'image files', 'text files']
-
-for loop in range(3):
-    if not os.path.exists(path + folder_names[loop]):
-        print(path + folder_names[loop])
-        os.makedirs(path + folder_names[loop])
-
-for file in file_name:
-    if '.csv' in file and not os.path.exists(path + 'csv files' + file):
-        shutil.move(path + file, path + 'csv files/' + file)
-    elif '.png' in file and not os.path.exists(path + 'image files' + file):
-        shutil.move(path + file, path + 'image files/' + file)
-    elif '.txt' in file and not os.path.exists(path + 'text files' + file):
-        shutil.move(path + file, path + 'text files/' + file)
+for file_name in file_names:
+    file_path = os.path.join(path, file_name)
+    if file_name.endswith('.pdf') and not os.path.exists(os.path.join(path, 'Pdf files', file_name)):
+        shutil.move(file_path, os.path.join(path, 'Pdf files', file_name))
+    elif file_name.endswith('.png') and not os.path.exists(os.path.join(path, 'Image files', file_name)):
+        shutil.move(file_path, os.path.join(path, 'Image files', file_name))
+    elif file_name.endswith('.txt') and not os.path.exists(os.path.join(path, 'Text files', file_name)):
+        shutil.move(file_path, os.path.join(path, 'Text files', file_name))
+    elif file_name.endswith('.mp4') and not os.path.exists(os.path.join(path, 'Tuto files', file_name)):
+        shutil.move(file_path, os.path.join(path, 'Tuto files', file_name))
+    elif file_name.endswith('.exe') and not os.path.exists(os.path.join(path, 'APK files', file_name)):
+        shutil.move(file_path, os.path.join(path, 'APK files', file_name))
     else:
-        print("There are files in this path that were not moved")
+        if file_name not in folder_names:
+            print(f"The file '{file_name}' was not moved.")
+        
+print("Operation completed.")
+        
